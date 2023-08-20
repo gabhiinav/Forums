882a5cbf4fd4e8bc3344d2902320c746c15f4cac
gabhiinav <gabhiinav@gmail.com>
2023-08-20 16:39:50 +0530

    Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,32 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Forums",
+  description: "Reddit-Clone",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialised">
+        <Navbar />
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
