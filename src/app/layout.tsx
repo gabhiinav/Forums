5d8dbb20ccafde2bd9a0058b62324592a1e49853
gabhiinav <gabhiinav@gmail.com>
2023-08-20 17:00:38 +0530

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar"
 
 export const metadata = {
   title: "Forums",
@@ -25,7 +26,7 @@ export default function RootLayout({
       <body className="min-h-screen pt-12 bg-slate-50 antialised">
         <Navbar />
 
-        {children}
+        <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
       </body>
     </html>
   );
